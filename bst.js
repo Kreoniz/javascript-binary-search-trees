4774663540acf47249cb822227e7c58a15de0c49
Kreoniz <kreoniz@mail.ru>
2024-02-16 17:41:41 +0600

    Create a method for building a balanced BST

---
+++ bst.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = this.buildTree(array);
+  }
+
+  buildTree(array) {
+    if (array.length === 0) {
+      return null;
+    }
+
+    const filtered = [...new Set(array.slice().sort((a, b) => a - b))];
+    const mid = parseInt(filtered.length / 2);
+
+    const root = new Node(filtered[mid]);
+    root.left = this.buildTree(filtered.slice(0, mid));
+    root.right = this.buildTree(filtered.slice(mid + 1, filtered.length - 1));
+    
+    return root;
+  }
+}
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+
+ const prettyPrint = (node, prefix = "", isLeft = true) => {
+   if (node === null) {
+     return;
+   }
+   if (node.right !== null) {
+     prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+   }
+   console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+   if (node.left !== null) {
+     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+   }
+ };
+
+console.log(tree.buildTree(array));
+console.log();
+prettyPrint(tree.buildTree(array));
